52bd94de6a2a1bdf92171238b316dd81d37ac0c4
Cyc <topcoder.cyc@gmail.com>
2025-11-07 11:16:21 +0800

    feat: data input Analytics Page

--- Data/GameState.cs
+++ Data/GameState.cs
@@ -2,19 +2,49 @@ namespace Hackathon.Data
 {
     public class GameState
     {
+        // Basic Variables
         public int Money { get; set; } = 1000;
         public int EcoScore { get; set; } = 0;
         public int SocialScore { get; set; } = 0;
         public int Week { get; set; } = 1;
         public int PlayerIndex { get; set; } = 0;
 
+
+        // Weekly Record System
+        public class WeeklyRecord
+        {
+            public int Money { get; set; }
+            public int EcoScore { get; set; }
+            public int SocialScore { get; set; }
+        }
+
+        public List<WeeklyRecord> History { get; set; } = new();
+
+        public GameState() { RecordWeek(); }
+
+        public void RecordWeek()
+        {
+            History.Add(new WeeklyRecord
+            {
+                Money = Money,
+                EcoScore = EcoScore,
+                SocialScore = SocialScore
+            });
+        }
+
+
+        // Reset
         public void Reset()
         {
             Money = 1000;
             EcoScore = 0;
             SocialScore = 0;
             Week = 1;
+
+            History.Clear();
+            RecordWeek();
         }
+
 
         // Card Collection System
         public List<CardInfo> CollectedCards { get; set; } = new();
