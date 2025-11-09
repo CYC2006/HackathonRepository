b0eda2fa769723ecb64d6497a8c0d8174f5213a2
Cyc <topcoder.cyc@gmail.com>
2025-11-09 12:03:14 +0800

    feat: EventCardData & EventCardDESC initialize

---
+++ Data/EventCardDESC.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace Hackathon.Data
+{
+    public static class EventCardDESC
+    {
+        // 每個事件的卡片敘述列表
+        public static readonly Dictionary<string, List<string>> Desc = new()
+        {
+            ["四季"] = new List<string>
+            {
+                "春暖花開吸引遊客，地方商機上升但交通擁擠。",
+            },
+            
+        };
+    }
+}
---
+++ Data/EventCardData.cs
@@ -0,0 +1,80 @@
+using System.Collections.Generic;
+
+namespace Hackathon.Data
+{
+    public class EventCardInfo
+    {
+        public string Description { get; set; } = "";
+        public int MoneyChange { get; set; }
+        public int EcoChange { get; set; }
+        public int SocialChange { get; set; }
+        public int StartMonth { get; set; }
+        public int EndMonth { get; set; }
+        public string? ImagePath { get; set; } = null;
+        public string Type { get; set; } = "";
+
+        public EventCardInfo Clone()
+        {
+            return new EventCardInfo
+            {
+                Description = this.Description,
+                MoneyChange = this.MoneyChange,
+                EcoChange = this.EcoChange,
+                SocialChange = this.SocialChange,
+                StartMonth = this.StartMonth,
+                EndMonth = this.EndMonth,
+                ImagePath = this.ImagePath,
+                Type = this.Type,
+            };
+        }
+    }
+    
+    public static class EventCardData
+    {
+        public record Row(string Type, int Index, int Money, int Eco, int Social, int Start, int End, string ImagePath);
+
+        private static readonly Row[] Rows = new Row[]
+        {
+            new Row("四季", 0, 50, 20, 15, 2, 5, " "),
+        };
+
+        public static List<EventCardInfo> GetCardsForCity(string type)
+        {
+            var result = new List<EventCardInfo>();
+
+            string CityToFolder(string c) => c switch
+            {
+                "四季" => "season",
+                "天災" => "disaster",
+                "政策" => "policy",
+                "經濟" => "economics",
+                "環境" => "environments",
+                "社會" => "social",
+                _ => c
+            };
+
+            foreach (var row in Rows)
+            {
+                if (row.Type == type)
+                {
+                    var folder = CityToFolder(type);
+                    var img = $"/images/events/{folder}/{folder}{row.Index}.png";
+
+                    var card = new EventCardInfo
+                    {
+                        Description = EventCardDESC.Desc[type][row.Index],
+                        MoneyChange = row.Money,
+                        EcoChange = row.Eco,
+                        SocialChange = row.Social,
+                        ImagePath = img,
+                        Type = type
+                    };
+
+                    result.Add(card);
+                }
+            }
+
+            return result;
+        }
+    }
+}
