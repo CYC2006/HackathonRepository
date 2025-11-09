aaccaffa2e971305164c07f4c86b10f72c1fca35
Cyc <topcoder.cyc@gmail.com>
2025-11-09 13:25:27 +0800

    chore: all cities images added

--- Data/SustainCardData.cs
+++ Data/SustainCardData.cs
@@ -16,6 +16,10 @@ namespace Hackathon.Data
         public static readonly List<SustainEffect> Effects = new()
         {
             new SustainEffect { Til = "台積電宣布設廠", Tar = "Money", Min = 100, Max = 200, Dur = -1 },
+            new SustainEffect { Til = "中央山脈現稀土礦脈", Tar = "Money", Min = 300, Max = 500, Dur = 4 },
+            new SustainEffect { Til = "中央山脈現稀土礦脈", Tar = "EcoScore", Min = -20, Max = -10, Dur = -4 },
+            new SustainEffect { Til = "發現史前巨型遺址", Tar = "SocialScore", Min = 30, Max = 35, Dur = 8 },
+            new SustainEffect { Til = "國際AI總部進駐", Tar = "Money", Min = 100, Max = 200, Dur = -1 },
         };
 
         public static SustainEffect? GetEffect(string title)
